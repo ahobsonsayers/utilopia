43c9466ef65fb4959c74c17274bde0f90538f36c
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2024-12-29 15:36:34 +0000

    Initial commit

---
+++ envutils/env.go
@@ -0,0 +1,27 @@
+package envutils
+
+import (
+	"os"
+	"strconv"
+	"testing"
+)
+
+func IsCI() bool {
+	env, ok := os.LookupEnv("CI")
+	if !ok {
+		return false
+	}
+
+	isCI, err := strconv.ParseBool(env)
+	if err != nil {
+		return false
+	}
+
+	return isCI
+}
+
+func SkipIfCI(t *testing.T, args ...any) {
+	if IsCI() {
+		t.Skip(args...)
+	}
+}
